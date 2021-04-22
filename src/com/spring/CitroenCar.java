41414934212bafe0f572a3535d31e20ecb2a8b4d
mikedr <derosamiguel@gmail.com>
2021-04-22 00:32:01 -0300

    Inversion of Control (IoC)

---
+++ src/com/spring/CitroenCar.java
@@ -0,0 +1,8 @@
+package com.spring;
+
+public class CitroenCar {
+	
+	public String getAcarRide() {
+		return "Driving a Citroën car";
+	}
+}
---
+++ src/com/spring/MyApp.java
@@ -0,0 +1,10 @@
+package com.spring;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		CitroenCar car = new CitroenCar();
+		System.out.println(car.getAcarRide());
+	}
+
+}
