d824368df41aab27ff03209afe47038cd5c78b26
mikedr <derosamiguel@gmail.com>
2021-04-23 00:57:18 -0300

    IOC. Retrieve Beans from Spring Container

--- src/com/spring/MyApp.java
+++ src/com/spring/MyApp.java
@@ -6,6 +6,9 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Car aCar = context.getBean("myCar", Car.class);
+		System.out.println(aCar.getAcarRide());
+		context.close();
 	}
 
 }
