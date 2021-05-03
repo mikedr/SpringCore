fafbae27fb3255da6eb026e7725b2d3fd224fcd0
mikedr <derosamiguel@gmail.com>
2021-05-03 00:01:47 -0300

    Spring Bean Scope. Example of the default scope (Singleton)

--- src/com/spring/MyApp.java
+++ src/com/spring/MyApp.java
@@ -5,12 +5,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MyApp {
 
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		FordCar aCar = context.getBean("myNewestCar", FordCar.class);
-		System.out.println(aCar.getAcarRide());
-		System.out.println(aCar.getEngine());
-		System.out.println(aCar.getBrand());
-		System.out.println(aCar.getModel());
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		Car aCar = context.getBean("myCar", Car.class);
+		Car otherCar = context.getBean("myCar", Car.class);
+		boolean sameObject = (aCar == otherCar);
+		System.out.println("Are both variables pointing to the same object: "+sameObject);
+		System.out.println("Memory location of the first retrieved car: "+aCar);
+		System.out.println("Memory location of the second retrieved car: "+otherCar);
 		context.close();
 	}
 
