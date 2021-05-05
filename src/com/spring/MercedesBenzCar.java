8886bc189f752029fd23c476053412bdb4a8e552
mikedr <derosamiguel@gmail.com>
2021-05-05 00:02:54 -0300

    Annotations. Add the @Component annotation to the java classes 2

--- src/com/spring/MercedesBenzCar.java
+++ src/com/spring/MercedesBenzCar.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component("theMechi")
 public class MercedesBenzCar implements Car {
 
-	public MercedesBenzCar(EngineService engineService) {
+	public MercedesBenzCar() {
 
 	}
 
