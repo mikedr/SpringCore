eed21c7b658e34d5214ad672c8b46f348fe9c04f
mikedr <derosamiguel@gmail.com>
2021-05-22 20:49:27 -0300

    Qualifiers for Dependency. Fixing the error with @Qualifier

--- src/com/spring/MazdaCar.java
+++ src/com/spring/MazdaCar.java
@@ -1,12 +1,14 @@
 package com.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MazdaCar implements Car{
 
 	@Autowired
+	@Qualifier("straightFourEngine")
 	private EngineService engineService;
 	
 	public MazdaCar () {
