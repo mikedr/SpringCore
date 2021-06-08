b7fbc7ef3d25a1c124e28061f4c861a5509c2f2e
mikedr <derosamiguel@gmail.com>
2021-06-08 01:31:00 -0300

    Bean lifecycle with annotations. Example-2

--- src/com/spring/FiatCar.java
+++ src/com/spring/FiatCar.java
@@ -3,11 +3,9 @@ package com.spring;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
 public class FiatCar implements Car {
 
 	@Override
