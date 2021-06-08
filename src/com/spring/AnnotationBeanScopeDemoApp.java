9d5527144895e7c61a20c4cf8c55092d61bc40e8
mikedr <derosamiguel@gmail.com>
2021-06-08 00:52:27 -0300

    Bean scopes with annotations. Example of default scope

--- src/com/spring/AnnotationBeanScopeDemoApp.java
+++ src/com/spring/AnnotationBeanScopeDemoApp.java
@@ -1,10 +1,17 @@
 package com.spring;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AnnotationBeanScopeDemoApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Car theCar = context.getBean("myCar",Car.class);
+		Car otherCar = context.getBean("myCar",Car.class);
+		boolean result = theCar == otherCar;
+		System.out.println("Are pointing to the same object?: "+result);
+		context.close();
 	}
 
 }
