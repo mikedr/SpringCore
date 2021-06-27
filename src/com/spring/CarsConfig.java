53c3a92dd8cb483c6bc7af93a8adfff1803a04aa
mikedr <derosamiguel@gmail.com>
2021-06-27 00:35:38 -0300

    1.Define method to expose bean

--- src/com/spring/CarsConfig.java
+++ src/com/spring/CarsConfig.java
@@ -1,10 +1,16 @@
 package com.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.spring")
 public class CarsConfig {
+	
+	@Bean
+	public EngineService inlineEngine() {
+		return new InlineEngine();
+	}
 
 }
